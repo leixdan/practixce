dba51c133d3a7389924a45488f3484b6ff41dddc
IX <galvezbaezjd@gmail.com>
2025-02-08 02:43:50 +0000

    err

---
+++ Codespaces/Datos/IfElse/Doble/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+//Quiero hacer una condicional múltiple, haremos la de estatura y pesos para una montaña rusa
+
+func main(){
+
+	fmt.Println("Bienvenido a la montaña rusa Gaskull")
+	fmt.Println("¿Cuántos años tienes")
+	var edad int
+	fmt.Scan(&edad)
+	fmt.Println("¿Cuántos pinchos tienes chiko")
+	var pinchos int
+
+}
