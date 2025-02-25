3cc8d7f44b5b4d6d7d0748c0b4039b77c4bfd9d7
IX <galvezbaezjd@gmail.com>
2025-02-25 01:33:36 +0000

    Funciones

--- Codespaces/Datos/Mapas/main.go
+++ Codespaces/Datos/Mapas/main.go
@@ -1,13 +1,34 @@
 package main
 
-import (
-	"fmt"
-)
+import 	"fmt"
+
+func saludar (){ //Función sin parámetros y sin retorno
+	fmt.Println("Hola, este es un saludo de función simple")
+}
+
+func bienvenida (nombre string){ //Función con parámetros sin retorno
+	fmt.Printf("Bienvenido, %s\n",nombre)
+}
+
+func confirmarNombre (nombre *string){ //Función con parámetros sin retorno, con puntero
+	*nombre = "Mr bombastic" 
+	
+}
+
+func edadUsuario (a int, b int) int{
+	return a-b
+}
 
 func main(){
 
-	for {
-		fmt.Println("Tengo sueño")
-	}
+	saludar()
 
+	nombre := "Lamar"
+	bienvenida(nombre)
+
+	confirmarNombre(&nombre)
+	fmt.Printf("Sólo para estar seguros, ¿tu nombre es: %s?\n", nombre)
+
+	edad := edadUsuario(2025,1995)
+	fmt.Println("La edad del susodicho: ",edad)
 }
