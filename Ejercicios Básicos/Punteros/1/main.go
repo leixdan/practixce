9cb6cdec49a3f0202d14c79f86bb0d69fee5b0f7
leixdan <galvezbaezjd@gmail.com>
2025-04-13 21:45:55 -0600

    Ejercicios punteros

---
+++ Ejercicios Básicos/Punteros/1/main.go
@@ -0,0 +1,31 @@
+//Ejercicios mamalones de punteros porque estoy bien wey y se me olvidan las funciones tambien
+
+package main
+
+import "fmt"
+
+func duplicar(n int) int { //Ej 1
+	return n * 2
+}
+
+func duplicarPuntero(n *int) { //Ej 2
+	*n = *n * 3
+}
+
+func main() {
+
+	//Variables
+
+	var dup int
+
+	//Ejercicio 1
+	fmt.Println("Ingresa el número que deseas duplicar: ")
+	fmt.Scan(&dup)
+	resultado := duplicar(dup)
+	fmt.Println("El resultado es:", resultado)
+
+	//Ejercicio 2
+	fmt.Println("Número triplicado por puntero: ")
+	duplicarPuntero(&dup)
+	fmt.Println("El resultado es:", dup)
+}
