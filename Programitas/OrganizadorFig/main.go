5cd6456ac9f2ea81cfcdc8504ea438fcb50a4169
IX <galvezbaezjd@gmail.com>
2025-04-27 18:50:29 +0000

    Se agrega triángulo como nueva figura

--- Programitas/OrganizadorFig/main.go
+++ Programitas/OrganizadorFig/main.go
@@ -24,6 +24,14 @@ func (c Circulo) Area() float64 { // Implementación del método Area para Círc
 	return 3.14 * c.Radio * c.Radio
 }
 
+// Triángulo
+type Triangulo struct{
+	Base, Alto float64
+}
+func (t Triangulo) Area() float64{
+	return t.Base * t.Alto / 2
+}
+
 // Función que recibe una figura y devuelve su área
 func ImprimirArea(f Figura) { //Se usa la interfaz Figura como tipo de parámetro
 	// Se puede pasar cualquier figura que implemente el método Area
@@ -34,8 +42,10 @@ func main() {
 
 	r := Rectangulo{Ancho: 10, Alto: 5}
 	c := Circulo{Radio: 3}
+	t := Triangulo{Base: 9, Alto: 10}
 
 	ImprimirArea(r)
 	ImprimirArea(c)
+	ImprimirArea(t)
 
 }
