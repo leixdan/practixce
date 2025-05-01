b38dc3d245483731ff2ee681c2c9d8602ac54d05
leixdan <galvezbaezjd@gmail.com>
2025-05-01 08:46:07 -0600

    Citas médicas, arquitectura del programa y primeros pasos

---
+++ Programitas/Citasmed/agenda/agenda.go
@@ -0,0 +1,3 @@
+//Aquí vamos a guardar los types de las citas para el día
+
+package agenda
---
+++ Programitas/Citasmed/main.go
@@ -0,0 +1,12 @@
+//Este programa usará los otros archivos a modo de módulos para poder correr
+//Se deberá tener el menú de acceso a los módulos: 1. Consulta de citas, 2. Lista de consultorios, 3. Personas en espera y 4. Salir
+//En 1 deberá dar las opciones de eliminar una cita programada y colocar a una persona en espera, deberá descontar la persona en espera
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Bienvenido al consultorio médico")
+}
