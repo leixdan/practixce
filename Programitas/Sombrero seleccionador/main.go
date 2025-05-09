8f0a32cae740c26d95ee1ef23bc1bc7724cddda8
leixdan <galvezbaezjd@gmail.com>
2025-05-09 17:33:34 -0600

    Fin del programa

--- Programitas/Sombrero seleccionador/main.go
+++ Programitas/Sombrero seleccionador/main.go
@@ -69,11 +69,19 @@ func main() {
 		preguntas.SumarRespuesta(&puntaje, &respuesta)
 	}
 
-	fmt.Println(puntaje.A, puntaje.B, puntaje.C, puntaje.D)
+	fmt.Printf("Bien, ya tengo tu respuesta %s\n", name)
+	
 	//Ya se calcula el resultado, ahora toca que compare y sólo muestre el de mayor puntaje.
 
 	if puntaje.A > puntaje.B && puntaje.A > puntaje.C && puntaje.A > puntaje.D {
-		fmt.Printf()
+		fmt.Println("Eres todo un Gryffindor, ve a matar basiliscos y ganar puntos por respirar")
+	} else if puntaje.B > puntaje.A && puntaje.B > puntaje.C && puntaje.B > puntaje.D {
+		fmt.Println("Eres todo un Ravenclaw, no serás relevante más allá de... nada")
+	} else if puntaje.C > puntaje.A && puntaje.C > puntaje.B && puntaje.C > puntaje.D {
+		fmt.Println("Eres todo un Hufflepuff, ve a abrazar a algún arbol... hippie")
+	} else if puntaje.D > puntaje.A && puntaje.D > puntaje.B && puntaje.D > puntaje.C {
+		fmt.Println("Eres todo un Slytherin, culebra, largo de mi vista")
+	} else {
+		fmt.Println("No tienes casa, eres un tibio")
 	}
-
 }
