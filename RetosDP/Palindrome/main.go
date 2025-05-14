1478c663ad6503354d996936f7172da8275beb14
leixdan <galvezbaezjd@gmail.com>
2025-05-14 08:22:40 -0600

    No era anagrama, es palíndromo

---
+++ RetosDP/Palindrome/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+/*func revisar(palabra *string) {
+
+	for i, l := range *palabra {
+		fmt.Printf("Índice: %d Letra: %c\n", i, l)
+	}
+}*/
+
+func main() {
+
+	var palabra string
+
+	fmt.Println("Vamos a ver si tu palabra es un anagrama")
+	fmt.Println("Ingresa tu palabra")
+	fmt.Scan(&palabra)
+	//revisar(&palabra)
+
+	slice_palabra := []rune(palabra)
+
+	for i, l := 0, len(slice_palabra)-1; i < l; i, l = i+1, l-1 {
+		fmt.Printf("i: %d, l:%d\n", slice_palabra[i], slice_palabra[l])
+	}
+
+}
