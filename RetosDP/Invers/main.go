11d3ea822e6bae35ee7cc66ae19a4e21508ba0fd
IX <galvezbaezjd@gmail.com>
2025-05-16 00:20:32 +0000

    Se invierte un string

---
+++ RetosDP/Invers/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main (){
+
+	var solicitud string
+
+	fmt.Println("Vamos a invertir el texto que me pases")
+	fmt.Println("Ingresa la palabra que deseas invertir:")
+	fmt.Scan(&solicitud)
+
+	sSolicitud := []rune (solicitud)
+	var invertido string
+	
+	for p := len(sSolicitud)-1; p >= 0; p--{
+		invertido += string(sSolicitud[p])
+	}
+
+fmt.Println(invertido)
+
+}
