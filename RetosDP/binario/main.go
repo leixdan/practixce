fa92d89053668acb94dd4c7bfa18205c8735ecf6
IX <galvezbaezjd@gmail.com>
2025-05-29 00:56:52 +0000

    Se termina el programita de convertidor a binario

--- RetosDP/binario/main.go
+++ RetosDP/binario/main.go
@@ -5,19 +5,32 @@ import "fmt"
 
 func main(){
 
-	var respuesta int = 25
+	var respuesta int
 	var sBinario []int
-	var binario int
 
 	fmt.Println("Vamos a convertir tu número")
+	fmt.Println("Ingresa el número que deseas convertir a binario: ")
+	fmt.Scan(&respuesta)
 	
 	i := respuesta
 	for i > 0 {
-		
-		i = i/2
+		if i % 2 == 0 {
+			sBinario = append(sBinario, 0)
+		} else {
+			sBinario = append(sBinario, 1)
 		}
+		i = i/2
 
+		//sBinario = append(sBinario, i)
+		}
+		var invertido string
+		for i := len(sBinario) - 1; i >= 0; i--{
+//Se hace uso de Sprintf para convertir los números
+//a strings ya que si se usa string suma los 1 y arroja
+//espacios vacíos, no es lo buscado
+			invertido += fmt.Sprintf("%d",sBinario[i])
+		}
 		fmt.Println("Terminó el ciclo")
-		fmt.Println(sBinario)
+		fmt.Printf("Tu número en binario es: %s\n",invertido)
 
 }
