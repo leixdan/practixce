83c78adeaa47f55b34b0c75dd618c38f06127c84
IX <galvezbaezjd@gmail.com>
2025-06-14 18:14:45 +0000

    Revisión del ciclo anidado

--- RetosDP/elim/main.go
+++ RetosDP/elim/main.go
@@ -5,56 +5,16 @@
 
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-//var Sout1, Sout2 string
-
-func convers(sOut1 []rune, sOut2 []rune, outArmado1 *string) {
-
-	for _,r1 := range sOut1 {
-		for _,r2 := range sOut2{
-
-			if r1 == r2{
-				*outArmado1 += string(r1)
-				break
-			}
-	
-		}
-}
-}
-
-func main() {
-
-	var str1, str2 string = "caramelo", "camaron"
-	sOut1 := []rune(str1)
-	sOut2 := []rune(str2)
-	
-	var outArmado1 string
-
-	//fmt.Println("Ingresa las dos palabras")
-	fmt.Println("Caramelo: ", sOut1)
-	fmt.Println("Camaron: ",sOut2)
-
-	convers(sOut1, sOut2, &outArmado1)
-fmt.Println("_________________________")
-
-fmt.Println(outArmado1)
-
-
-}
-
-
-/*
-
-func convers(sOut1 []rune, sOut2 []rune) string {
-	var resultado string
-	for _, r1 := range sOut1 {
-		for _, r2 := range sOut2 {
-			if r1 == r2 {
-				resultado += string(r1)
-				break
+func convers(sOut1 []rune, sOut2 []rune) string {	//La función recibe dos runes y devuelve un string
+	var resultado string							//Se declara una variable local de la función
+	for _, r1 := range sOut1 {						//Se recorre r1(rune1)
+		for _, r2 := range sOut2 {					//Ciclo anidado de r2(rune2)
+			if r1 == r2 {							//Se compara el recorrido de r1 y r2
+				resultado += string(r1)				//Se guarda en la variable temporal resultado si son iguales
+				fmt.Println(resultado)
+				break								
 			}
 		}
 	}
@@ -71,5 +31,5 @@ func main() {
 	fmt.Println("_________________________")
 	fmt.Println(outArmado1)
 }
-	*/
+
 
