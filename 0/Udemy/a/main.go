6513f8b2946d1106b638ed36d77def1e02ae80f7
leixdan <galvezbaezjd@gmail.com>
2025-09-21 16:17:21 -0600

    Jueguito1

---
+++ 0/Udemy/a/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	fmt.Println("Bienvenido al juego de adivinar")
+	fmt.Println("y/n")
+	var respuesta string
+	fmt.Scanln(&respuesta)
+
+	if respuesta == "y" {
+		jugar()
+	}
+
+	fmt.Println("Adiós")
+
+}
+
+func jugar() {
+	numRand := rand.Intn(100)
+	var numIngresado int
+	var intentos int
+	const maxIntentos = 10
+
+	for intentos < maxIntentos {
+		intentos++
+		fmt.Printf("Ingresa un número (intentos restantes: %d): /n", maxIntentos-intentos+1)
+		fmt.Scanln(&numIngresado)
+
+		if numIngresado == numRand {
+			fmt.Println("Feliciddes, adivinaste el número.")
+			return
+		} else if numIngresado < numRand {
+			fmt.Println("El número a adivinar es mayor")
+		} else if numIngresado > numRand {
+			fmt.Println("El número a adivinar es menor")
+		}
+	}
+	fmt.Println("Se te acabaron los intentos, el número era: ", numRand)
+}
